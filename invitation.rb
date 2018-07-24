24b31c52db88085c04e6d485598087ae5ebab696
flippingflippers <citrussunshine2004@gmail.com>
2018-07-24 13:54:09 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,28 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+def guest_name
+  Dear Mathiass
+end
+
+def party_name
+  You are cor
+end
+
+def date
+  Please bring yourself on August 1st.
+end
+
+def time
+  All of the fun starts at 3pm!
+end
+
+def host_name
+  Sincerely, Alexina Hill
+end
+
+*guest_name
+*party_name
+*date
+*time
+*host_name
